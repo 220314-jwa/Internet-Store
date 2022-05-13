20c506323561e0294ede0ce3a2f1af07cc38e757
alex karas <alex4karas44@yahoo.com>
2022-05-13 02:04:35 -0400

    order service tests

--- e-commerce/src/main/java/com/synergy/ecommerce/repositories/OrderRepository.java
+++ e-commerce/src/main/java/com/synergy/ecommerce/repositories/OrderRepository.java
@@ -1,14 +1,25 @@
 package com.synergy.ecommerce.repositories;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import com.synergy.ecommerce.entities.Order
 ;
 public interface OrderRepository extends JpaRepository<Order, Long>{
 
-	Optional<Order> findOrderByOrderDate(Date orderDate);
+	List<Order> findOrderByOrderDate(Date orderDate);
+	
+	List<Order> getOrders();
+	
+	Order findOrderById(long Id);
+
+	Order getByOrderStatus(String orderStatus);
+	
+	Order addNewOrder(Order order);
+	
+	
+	
 }
