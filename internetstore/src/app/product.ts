0c3c23fbf4fa986842ecf3f6369c6bee10e823bd
Rustem <48765033+RustemCoder@users.noreply.github.com>
2022-05-12 19:23:13 -0500

    Delete internetstore directory

--- internetstore/src/app/product.ts
+++
@@ -1,9 +0,0 @@
-export class Product {
-    constructor(
-        public id: number,
-        public name: string,
-        public price: number,
-        public code: number,
-        public prod_date: Date,
-    ) {}
-}
